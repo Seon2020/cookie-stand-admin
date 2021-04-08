424257cf2eb2528884cb96f2b8ae9091642fcb3d
Sean <seanh@zillowgroup.com>
2021-04-08 00:05:41 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,17 @@ import { useState } from 'react'
 
 export default function Home() {
 
-  const [cookieStandNew, setNewCookieStand] = useState()
+  const [location, setLocation] = useState()
+  const [minCustomers, setMin] = useState()
+  const [maxCustomers, setMax] = useState()
+  const [avgCookies, setAvg] = useState()
 
   function cookieStandCreateHandler(event){
-    event.preventDefault();
-    const formData = new FormData(event.target)
-    const cookieStandNew = JSON.stringify(Object.fromEntries(formData), null, 3)
-    setNewCookieStand(cookieStandNew);
+    event.preventDefault()
+    setLocation(event.target.location.value)
+    setMin(event.target.minCustomers.value)
+    setMax(event.target.maxCustomers.value)
+    setAvg(event.target.avgCookies.value)
   }  
 
   return (
@@ -27,7 +31,7 @@ export default function Home() {
 
         <div className="text-gray-500 my-10 text-center">
           <p>Report Table Coming Soon...</p>
-          <p className="my-8">{cookieStandNew}</p>
+          <p className="my-8">{"{ "}"location": "{location}", "minCustomers": {minCustomers}, "maxCustomers": {maxCustomers}, "avgCookies": {avgCookies}{" }"}</p>
         </div>
 
       </main>
@@ -55,7 +59,7 @@ export default function Home() {
 
   function Form(props){
     return(
-      <form onSubmit={cookieStandCreateHandler} name="formData" className="w-9/12 m-auto p-4 my-4 rounded bg-green-300">
+      <form onSubmit={cookieStandCreateHandler} className="w-9/12 m-auto p-4 my-4 rounded bg-green-300">
         <h2 className="text-center text-2xl mb-4">{props.title}</h2>
         <div className="flex">
           <label for='location' className='text-sm font-bold pr-2'>Location</label>
@@ -71,8 +75,8 @@ export default function Home() {
             <input name='maxCustomers' className="text-black w-full"></input>
           </div>
           <div className="flex-1">
-            <label className='text-sm font-bold' for='avgCustomers'>Average Cookies per Sale</label>
-            <input name='avgCustomers' className="text-black w-full"></input>
+            <label className='text-sm font-bold' for='avgCookies'>Average Cookies per Sale</label>
+            <input name='avgCookies' className="text-black w-full"></input>
           </div>
           <div className='flex-1 self-stretch'>
             <button type="submit" className="w-full h-full text-black bg-green-500">Create</button>
