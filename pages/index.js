234c3bd691c978b14d83c71ebc864326a83c3b26
Sean <seanh@zillowgroup.com>
2021-04-07 22:51:08 -0700

    finished

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,86 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [cookieStandNew, setNewCookieStand] = useState()
+
+  function cookieStandCreateHandler(event){
+    event.preventDefault();
+    const formData = new FormData(event.target)
+    const cookieStandNew = JSON.stringify(Object.fromEntries(formData), null, 3)
+    setNewCookieStand(cookieStandNew);
+  }  
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className='flex flex-col min-h-screen py-5'>
       <Head>
-        <title>Create Next App</title>
+        <title>Cookie Stand App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <Header title='Cookie Stand Admin'/>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      <main className='bg-green-50 h-full'>
+  
+        <Form title="Create Cookie Stand"/>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        <div className="text-gray-500 my-10 text-center">
+        <p>Report Table Coming Soon...</p>
+        <p className="my-8">{cookieStandNew}</p>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+      </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Footer title='&copy; 2021' />
     </div>
   )
+
+
+  function Header(props){
+    return(
+      <header className="bg-green-500 text-black p-4 border">
+        <h1 className="text-4xl">{props.title}</h1>
+      </header>
+    )
+  }
+
+  function Footer(props){
+    return(
+      <footer className="bg-green-500 text-black p-4">
+        <p className="">{props.title}</p>
+      </footer>
+    )
+  }
+
+  function Form(props){
+    return(
+      <form onSubmit={cookieStandCreateHandler} name="formData" className="w-9/12 m-auto p-4 my-4 rounded bg-green-300">
+        <h2 className="text-center text-2xl mb-4">{props.title}</h2>
+        <div className="flex">
+          <label for='location' className='text-sm font-bold pr-2'>Location</label>
+          <input name='location' className="text-black flex-1"></input>
+        </div>
+        <div className="flex items-end gap-x-3 pt-8">
+          <div className="flex-1">
+            <label className='text-sm font-bold' for='min'>Minimum Customers per Hour</label>
+            <input name='min' className="text-black w-full"></input>
+          </div>
+          <div className="flex-1">
+            <label className='text-sm font-bold' for='max'>Maximum Customers per Hour</label>
+            <input name='max' className="text-black w-full"></input>
+          </div>
+          <div className="flex-1">
+            <label className='text-sm font-bold' for='avg'>Average Cookies per Sale</label>
+            <input name='avg' className="text-black w-full"></input>
+          </div>
+          <div className='flex-1 self-stretch'>
+            <button type="submit" className="w-full h-full text-black bg-green-500">Create</button>
+          </div>
+        </div>
+      </form>
+    )
+  }
 }
+
